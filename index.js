ca09df8e81d5107af029611c0dc1983c84cfa808
lujansanchez <lsanchez@visual-engin.com>
2021-07-10 15:56:20 +0200

    added slick and carousel

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        autoplay: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: false,
+        variableWidth: true,
+        mobileFirst: true,
+        respondTo: 'min',
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                centerMode: true,
+              }
+            },
+        ]
+    });
+  });
